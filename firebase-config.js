2bb6c526592becc252d971d4d34cf1aa248ae6ab
fingerprintacoustic <fingerprintacoustic@gmail.com>
2026-07-15 17:16:31 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase config for the Prime Broilers project
+const firebaseConfig = {
+  apiKey: "AIzaSyDKxZsNHvJKdp9cPLVJRkmkQ1sQklpxV9Q",
+  authDomain: "prime-broilers.firebaseapp.com",
+  projectId: "prime-broilers",
+  storageBucket: "prime-broilers.firebasestorage.app",
+  messagingSenderId: "588544628633",
+  appId: "1:588544628633:web:05ba9d298a768fca176508"
+};
